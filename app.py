7245dc73b9356a9ce1e4f2601b76cf6050721bc2
Garden <prepella3@gmail.com>
2021-06-08 10:02:18 +0900

    modified app.py
    app.py에 html 파일들 연결,
    html 파일들에 인식용 h1태그 작성
    
    index.html 에러 제거

--- app.py
+++ app.py
@@ -9,48 +9,77 @@ db = client.dbsparta
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+	return render_template('index.html')
 
 
 @app.route('/main')
 def main():
-    return render_template('main.html')
+	return render_template('main.html')
 
 
 @app.route('/mypage')
 def mypage():
-    return render_template('mypage.html')
+	return render_template('mypage.html')
 
 
 @app.route('/detail')
 def detail():
-    return render_template('pension_detail.html')
+	return render_template('pension_detail.html')
+
+
+@app.route('/register')
+def register():
+	return render_template('register.html')
+
+
+@app.route('/login')
+def login():
+	return render_template('login.html')
 
 
 # API 역할을 하는 부분
-@app.route('/api/list', methods=['GET'])
+@app.route('/list', methods=['GET'])
 def show_list():
-    sample_receive = request.args.get('sample_give')
-    print(sample_receive)
+	pensions = list(db.mystar.find({}, {'_id': False}))
 
-    return jsonify({'msg': 'list 연결되었습니다!'})
-
-
-@app.route('/api/like', methods=['POST'])
-def modify_list():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-
-    return jsonify({'msg': 'like 연결되었습니다!'})
+	return jsonify({'pensions_give': pensions})
 
 
-@app.route('/api/delete', methods=['POST'])
-def delete_list():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
+@app.route('/detail/<pension_id>', methods=['GET'])
+def pension_detail(pension_id):
+	# id 값으로 불러오기
+	pension = db.mopen.find_one({'_id': pension_id}, {'_id': False})
 
-    return jsonify({'msg': 'delete 연결되었습니다!'})
+	# id 값으로 찾은 해당 펜션 정보(name, price, ...) 전부 전달
+	return jsonify({'pension_give': pension})
 
+
+# register, login 기능은 공부 후 추가하기로
+"""
+@app.route('/register', methods=['POST'])
+def register():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+
+    doc = {
+        'id': id_receive,
+        'pw': pw_receive
+    }
+
+    # 사용자 정보 db에 저장하기
+
+    return jsonify({'msg': '회원가입 성공'})
+"""
+
+# like 기능은 시간 남을 경우 추가하기로
+"""
+# like 기능 만들 경우 db에 like값 추가 필요(True/False)
+@app.route('/like/<pension_id>', methods=['POST'])
+def like(pension_id):
+	db.mopen.update_one({'_id': pension_id}, {'$set': {'like': True}})
+	
+	return jsonify({'msg': 'You liked this pension!'}) 
+"""
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+	app.run('0.0.0.0', port=5000, debug=True)
