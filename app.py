d32fe2e135dbf8489021fcb465916502f5d17069
Garden <prepella3@gmail.com>
2021-06-08 20:45:37 +0900

    add bgimage

--- app.py
+++ app.py
@@ -3,45 +3,54 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-#----- DB 연결 : 펜션 정보 -----#
+# ----- DB 연결 : 펜션 정보 -----#
 client = MongoClient('localhost', 27017)
 dbMopen = client.mopen
 colPensionInfo = dbMopen.pensionInfo
 
-#----- route -----#
+
+# ----- route -----#
 @app.route('/')
 def home():
-	return render_template('index.html')
+    return render_template('index.html')
 
 
-#-- Main: 펜션 목록 페이지 --#
+# -- Main: 펜션 목록 페이지 --#
 @app.route('/main/', methods=['GET'])
 def main():
-	pensions = list(colPensionInfo.find({}))
-	return render_template('main.html', pensions = pensions)
+    location = request.args["location"]
+
+    if location == "전체":
+        pensions = list(colPensionInfo.find({}))
+    else:
+        pensions = list(colPensionInfo.find({'location': location}))
+
+    return render_template('main.html', pensions=pensions)
+
 
 # # API 역할을 하는 부분
 # # 펜션 상세페이지
 @app.route('/detail/<path:pension_id>', methods=['GET'])
 def pension_detail(pension_id):
-	pension = colPensionInfo.find_one({'_id': pension_id})
-	# id 값으로 찾은 해당 펜션 정보(name, price, ...) 전부 전달
-	return render_template("pension_detail.html", pension = pension)
+    pension = colPensionInfo.find_one({'_id': pension_id})
+    # id 값으로 찾은 해당 펜션 정보(name, price, ...) 전부 전달
+    return render_template("pension_detail.html", pension=pension)
 
 
 @app.route('/mypage')
 def mypage():
-	return render_template('mypage.html')
+    return render_template('mypage.html')
 
 
 @app.route('/register')
 def register():
-	return render_template('register.html')
+    return render_template('register.html')
 
 
 @app.route('/login')
 def login():
-	return render_template('login.html')
+    return render_template('login.html')
+
 
 # register, login 기능은 공부 후 추가하기로
 """
@@ -66,9 +75,9 @@ def register():
 @app.route('/like/<pension_id>', methods=['POST'])
 def like(pension_id):
 	db.mopen.update_one({'_id': pension_id}, {'$set': {'like': True}})
-	
+
 	return jsonify({'msg': 'You liked this pension!'}) 
 """
 
 if __name__ == '__main__':
-	app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
