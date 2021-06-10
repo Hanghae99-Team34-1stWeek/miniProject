b0e6870b25dac19437b6b77e0fc879dd2d1cb0a3
Garden <prepella3@gmail.com>
2021-06-10 15:57:07 +0900

    remove menu in some urls

--- app.py
+++ app.py
@@ -98,7 +98,20 @@ def pension_detail(pension_id):
 
 @app.route('/mypage')
 def mypage():
-	return render_template('mypage.html')
+	token_receive = request.cookies.get('mytoken')
+
+	try:
+		payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+		user_info = colUser.find_one({"id": payload['id']})
+
+		return render_template('mypage.html', nickname=user_info["id"])
+	except jwt.ExpiredSignatureError:
+		return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+
+	except jwt.exceptions.DecodeError:
+		return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+
 
 
 @app.route('/register')
